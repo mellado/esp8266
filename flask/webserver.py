ed968c240ba547bc06cec888b9b9c613c099f7ed
mellado <mellado@pablomellado.es>
2017-12-30 21:20:25 +0100

    New function to get values in a range of dates
    The JSON in the file is not correct so I have to fix it.

--- flask/webserver.py
+++ flask/webserver.py
@@ -1,26 +1,44 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import json
 from flask import Flask
 from flask import request, jsonify
-from xively_interface import publish
 
-BEDROOM_PATH_LOG = "/home/osmc/esp8266/flask/bedroom.log"
+BEDROOM_PATH_LOG = "/tmp/bedroom.log"
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Hello world'
 
+@app.route('/dashboard')
+def dashboard():
+    pass
+
+def get_near_future_datetime():
+    result = datetime.now() + timedelta(minutes = 10)
+    return result
+
+@app.route('/get_data')
+def send_data():
+    from_datetime = request.args.get('from_datetime')
+    to_datetime = request.args.get('to_datetime', default=get_near_future_datetime())
+    print("from %s, to %s", (from_datetime, to_datetime))
+    raw = open(BEDROOM_PATH_LOG).read()
+    raw = raw.replace('}',"},\n")
+    raw = raw[:-2]
+    raw = "[\n" + raw + "\n]"
+    # data = json.load(open(BEDROOM_PATH_LOG))
+    data = json.loads(raw)
+    data = [i for i in data if i.get("time") >= from_datetime]
+    data = [i for i in data if i.get("time") <= to_datetime.strftime("%Y-%m-%d %H:%M:%S")]
+    raw = "%s - %s<br>%s - %s<br><pre>%s</pre>"% (from_datetime, to_datetime, data[0].get("time"), data[-1].get("time"), data)
+    return raw
+
 @app.route('/save_data', methods=['POST'])
 def save_data():
     error = None
     result = {'success':'ok'}
     content = request.get_json(silent=True)
-    print(content)
-    print(content['temperature'])
-    print(content['humidity'])
-    publish("humidity", "dormitorio", content['humidity'])
-    publish("temperature", "dormitorio", content['temperature'])
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     f = open(BEDROOM_PATH_LOG, "a")
     line = {'time': timestamp, 'temperature':content['temperature'],
